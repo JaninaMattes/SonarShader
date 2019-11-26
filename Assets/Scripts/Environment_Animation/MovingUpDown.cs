591f6672c0f515c0bfca08444ad4d99e8476c9fe
Janina Mattes <jm105@hdm-stuttgart.de>
2019-11-26 16:46:59 +0100

    sonar_shader inital commit

---
+++ Assets/Scripts/Environment_Animation/MovingUpDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingUpDown : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float height = 0.5f;
+    private Vector3 pos = new Vector3(0, 0, 0);
+
+    private void Start()
+    {
+        // Get the current objects possition
+        pos = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the new Y-position
+        float y_position = Mathf.Sin(Time.time * speed) * height + pos.y;
+        // Set the objects y to the calculatet y-position
+        transform.position = new Vector3(pos.x, y_position, pos.z) * height;
+
+        Debug.Log("Position y: " + y_position);
+    }
+}
---
+++ Assets/Scripts/Lidar_Shader/LidarRayCast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LidarRayCast : MonoBehaviour
+{
+    public float TargetDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+        // sending a raycast and out receives the raycast distance
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit))
+        {
+            TargetDistance = Hit.distance;
+        }
+    }
+}
